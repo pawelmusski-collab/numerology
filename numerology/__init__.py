8447581d205c28aec41d5e3bc4d317d004554f17
pawelmusski-collab <pawelmusski@gmail.com>
2026-04-30 01:07:27 +0200

    Update __init__.py

--- numerology/__init__.py
+++ numerology/__init__.py
@@ -1,11 +1,12 @@
 from .belova import calculate_belova_number, get_belova_description
-from .psychomatrix import calculate_psychomatrix, get_psychomatrix_summary, format_psychomatrix_table, psychomatrix_to_json
+from .psychomatrix import calculate_psychomatrix, get_psychomatrix_summary, psychomatrix_to_json
+from .image_gen import generate_psychomatrix_image
 
 __all__ = [
     "calculate_belova_number",
     "get_belova_description",
     "calculate_psychomatrix",
     "get_psychomatrix_summary",
-    "format_psychomatrix_table",
     "psychomatrix_to_json",
+    "generate_psychomatrix_image",
 ]
