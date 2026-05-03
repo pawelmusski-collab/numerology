3daf44e052c1daf712a9de75373a8e5bb4010142
pawelmusski-collab <pawelmusski@gmail.com>
2026-05-03 23:12:04 +0200

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,3 +1,3 @@
-from .db import init_db, get_or_create_user, update_user_numerology, create_booking, async_session
+from .db import init_db, get_or_create_user, get_user, update_user_numerology, create_booking, async_session
 
-__all__ = ["init_db", "get_or_create_user", "update_user_numerology", "create_booking", "async_session"]
+__all__ = ["init_db", "get_or_create_user", "get_user", "update_user_numerology", "create_booking", "async_session"]
