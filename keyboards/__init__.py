4ad22e14f1b75ca487cddacb4f0c54795304ac84
pawelmusski-collab <pawelmusski@gmail.com>
2026-05-04 12:56:36 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,3 +1,17 @@
-from .menus import get_main_menu, get_booking_time_menu, get_cancel_menu, get_confirm_booking_menu
+from .menus import (
+    get_main_menu,
+    get_booking_time_menu,
+    get_cancel_menu,
+    get_confirm_booking_menu,
+    get_ai_end_menu,
+    get_ai_confirm_menu,
+)
 
-__all__ = ["get_main_menu", "get_booking_time_menu", "get_cancel_menu", "get_confirm_booking_menu"]
+__all__ = [
+    "get_main_menu",
+    "get_booking_time_menu",
+    "get_cancel_menu",
+    "get_confirm_booking_menu",
+    "get_ai_end_menu",
+    "get_ai_confirm_menu",
+]
