f2b8d756c9b64bdfd0d4c483e9b440fa30f7d825
pawelmusski-collab <pawelmusski@gmail.com>
2026-05-04 12:49:58 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,6 @@
 from .start import router as start_router
 from .birthdate import router as birthdate_router
 from .booking import router as booking_router
+from .ai_chat import router as ai_router
 
-__all__ = ["start_router", "birthdate_router", "booking_router"]
+__all__ = ["start_router", "birthdate_router", "booking_router", "ai_router"]
